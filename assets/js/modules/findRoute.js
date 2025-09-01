18ad6430ac17a8fa50810250eff110c1b692581d
joaofernandoz <joaofernandooliveirazacarias@gmail.com>
2025-09-01 16:38:45 -0300

    adicionei ponto-virgula

--- assets/js/modules/findRoute.js
+++ assets/js/modules/findRoute.js
@@ -2,6 +2,6 @@ function findRoute(origin, destination, routes){
     const routeObject = routes.find(route => route.origem === origin && route.destino === destination);
     
     return routeObject;
-}
+};
 
-export default findRoute
+export default findRoute;
